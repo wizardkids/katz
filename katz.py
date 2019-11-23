ad01af72b6a95d3b367347a5f83981eb3152cc08
Rick Rawson <rick.rawson@outlook.com>
2019-11-22 20:39:57 -0500

    2019-11-22 08:39 PM -- fix a bunch of errors

--- katz.py
+++ katz.py
@@ -9,9 +9,11 @@ zip archiving program
 """
 
 import os
-import sys
+import shutil
 import zipfile
 from pprint import pprint
+
+# todo -- Review each function to see if it can be shortened
 
 
 def create_new():
@@ -35,7 +37,7 @@ def create_new():
         # create a new zip file
         with zipfile.ZipFile(file_name) as f:
             print('\n', file_name, 'created as new archive.\n')
-    return None
+    return file_name
 
 
 def get_filename():
@@ -82,21 +84,12 @@ def open_archive():
         zip_files = f.namelist()
         for ndx, file in enumerate(zip_files):
             print(ndx+1, '. ', file, sep='')
-    return
+    return file_name
 
 
 def add_file(file_name):
     """
-    Add one or more files to an existing archive:
-        1. pomodoro_I.py
-        2. pomodoro_II.py
-        3. Windows Notify.wav
-
-    becomes:
-        1. alert.txt
-        2. pomodoro_I.py
-        3. pomodoro_II.py
-        4. Windows Notify.wav
+    Add a file to an existing archive.
     """
     while True:
         # get a file name to add
@@ -109,60 +102,87 @@ def add_file(file_name):
         except:
             print('\nFile name is bad or file does not exist.\n')
 
-        with zipfile.ZipFile(file_name, 'a') as f:
-            f.write(file_to_add)
-    return
+        # determine if the file exists on disk
+        try:
+            with open(file_to_add, 'r') as f:
+                pass
+        except:
+            print('\nFile does not exist.')
+            break
+
+        # determine if the file already exists in the archive
+        with zipfile.ZipFile(file_name) as f:
+            zip_files = f.namelist()
+
+        if file_to_add in zip_files:
+            print(
+                '\nFile already exists in archive.\nRemove this file before adding a new version.')
+            break
+        else:
+            with zipfile.ZipFile(file_name, 'a') as f:
+                f.write(file_to_add)
+    return file_name
 
 
 def remove_file(file_name):
     """
     To remove a file, create a new archive with the removed file missing. This utility removes only one file at a time.
     """
-    # print a list of files in the archive
+    # check to see if the "temporary" directory already exists
+    if os.path.isdir(file_name[:-4]):
+        print('\nCannot remove files from archive, since ',
+              file_name[:-4], ' directory exists.', sep='')
+        return file_name
+
+    # for the user, print a list of files in the archive
     with zipfile.ZipFile(file_name, 'r') as f:
         zip_files = f.namelist()
         num_files = len(zip_files)
         for ndx, file in enumerate(zip_files):
             print(ndx+1, '. ', file, sep='')
 
-    # get the file that should be removed
+    # get from the user the file that should be removed
     while True:
-        list_files(file_name)
+        file_name = list_files(file_name)
         choice = input("\nEnter the number of file to remove: ")
+
+        if not choice.strip():
+            return file_name
+
+        # make sure user entered a valid integer
         try:
-            which_file = int(choice)
-            if which_file > num_files:
-                print('\nNumber must be in range 1-', num_files, sep='')
-                continue
-            else:
+            choice = int(choice)
+            if choice <= num_files:
                 break
         except:
-            print('\nMust enter an integer.')
+            print('\nEnter only an integer, in range 1-',
+                  num_files, '.\n', sep='')
             continue
 
     # print a list of files that the user has chosen, for confirmation
     with zipfile.ZipFile(file_name, 'r') as f:
         print()
         for ndx, file in enumerate(zip_files):
-            if ndx+1 == which_file:
+            if ndx+1 == choice:
                 print(ndx+1, '. ', file, sep='')
 
-    confirmed = input('\nRemove files? ').upper()
+    confirmed = input('\nRemove file? ').upper()
     if confirmed == 'Y':
         # extract all the files except the file user has chosen
         with zipfile.ZipFile(file_name, 'r') as f:
             zip_files = f.namelist()
+            this_path = '_' + file_name[:-4] + '_'
             for ndx, file in enumerate(zip_files):
-                if ndx+1 != which_file:
+                if ndx+1 != choice:
                     this_file = zip_files[ndx]
-                    f.extract(this_file, path=file_name[:-4])
+                    f.extract(this_file, path=this_path)
 
         current_directory = os.getcwd()
         rel_dir = os.path.relpath(current_directory, '')
 
         # add all the extracted files to _temp_zipfile_.zip
         with zipfile.ZipFile('_temp_zipfile_.zip', 'w') as f:
-            os.chdir(file_name[:-4])
+            os.chdir(this_path)
             # Iterate over all the files in directory
             for folderName, subfolders, filenames in os.walk(rel_dir):
                 for filename in filenames:
@@ -173,13 +193,16 @@ def remove_file(file_name):
 
         # change back to the directory holding file_name
         os.chdir(current_directory)
+
         # delete file_name
         os.remove(file_name)
 
         # rename _temp_zipfile_.zip to file_name
         os.rename('_temp_zipfile_.zip', file_name)
 
-    return
+        # delete the "temporary" directory
+        shutil.rmtree(this_path)
+    return file_name
 
 
 def list_files(file_name):
@@ -191,6 +214,7 @@ def list_files(file_name):
         zip_files = f.namelist()
         for ndx, file in enumerate(zip_files):
             print(ndx+1, '. ', file, sep='')
+
     return file_name
 
 
@@ -198,34 +222,42 @@ def extract_file(file_name):
     """
     Extract one or more files from an archive.
     """
-    # open the zip file and get a list of files
+    # get a list of the numbers for the files in the archive
     with zipfile.ZipFile(file_name, 'r') as f:
         zip_info = f.infolist()
-
-    # get a list of the number of files in the archive
     num_files = [str(x) for x in range(1, len(zip_info)+1)]
     num_files.append(' ')
 
     # allow user to choose which file(s) to extract
     while True:
-        list_files(file_name)
+        file_name = list_files(file_name)
+
         print(
-            "\nEnter the number of file to extract,\nor a comma-separated list of numbers.")
+            "\nEnter the number of file to extract,\na comma-separated list of numbers,\nor enter 'all' to extract all files.\n")
         choice = input("Choice: ")
-        which_files = choice.split(',')
+
+        if not choice.strip():
+            return file_name
+
+        which_files = [str(x) for x in range(
+            1, len(zip_info)+1)] if choice.strip().upper() == 'ALL' else choice.split(',')
+
         for i in which_files:
             if i.strip() not in num_files:
                 print('\nPlease use integers within the range: 1-',
                       num_files[-2], sep='')
                 print()
                 continue
+
         break
 
     which_files = [int(x) for x in which_files]
 
     # extract the files the user has chosen to path=file_name
     with zipfile.ZipFile(file_name, 'r') as f:
+
         zip_files = f.namelist()
+
         for ndx, file in enumerate(zip_files):
             if ndx+1 in which_files:
                 this_file = zip_files[ndx]
@@ -294,11 +326,11 @@ def sub_menu(open_file, new_file):
 
     while True:
         print()
-        print('<L>ist    <A>dd   <E>xtract\n<D>elete  <T>est  <R>eturn')
+        print('<L>ist    <A>dd   <E>xtract\n<R>emove  <T>est  <M>ain menu')
         print()
         user_choice = input('Choose an action: ').strip().upper()
 
-        if user_choice not in 'LAEDTR':
+        if user_choice not in 'LAERTM':
             print('\n"', user_choice, '" ', 'is an invalid action.', sep='')
             continue
 
@@ -312,7 +344,7 @@ def sub_menu(open_file, new_file):
         elif user_choice == 'E':
             file_name = extract_file(file_name)
 
-        elif user_choice == 'D':
+        elif user_choice == 'R':
             file_name = remove_file(file_name)
 
         elif user_choice == 'T':
