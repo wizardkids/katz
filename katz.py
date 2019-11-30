b0b476adc5ead584ddd1cba32a0f06f2d63148f1
Rick Rawson <rick.rawson@outlook.com>
2019-11-30 11:13:13 -0500

    2019-11-30 11:12 AM -- fix bug in remove_file()

--- katz.py
+++ katz.py
@@ -21,6 +21,10 @@ import zipfile
 from datetime import datetime
 from pathlib import Path
 
+# todo -- if <E>xtract will overwrite an existing file, provide user with a warning and a choice of what to do
+
+# todo -- if you remove the last file in the archive, the program exits unexpectedly (or generates an error)
+
 # todo -- version 2, add support for other archiving formats, including tar and gzip
 
 # todo -- version 3: add support for importing into other scripts so that downloaded archives are extracted automatically
@@ -410,6 +414,9 @@ def remove_file(full_path, file_name):
         print('\nCannot remove files from archive, since ',
               this_path, ' directory exists.', sep='')
         return full_path, file_name
+    # since it doesn't exist, create this_path
+    else:
+        os.mkdir(this_path)
 
     # get a list of files in the archive and their total number
     with zipfile.ZipFile(file_name, 'r') as f:
